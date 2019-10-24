c7e7a9a31bc47bc8a62a9c38431a9153326714fe
Bastien H <bastien.hiel@gmail.com>
2019-10-24 14:09:40 +0200

    first commit

---
+++ src/components/App/index.js
@@ -0,0 +1,24 @@
+/**
+ * Import
+ */
+import React from 'react';
+
+/**
+ * Local import
+ */
+// Composants enfants éventuels
+
+// Styles et assets
+import './app.sass';
+
+/**
+ * Code
+ */
+function Recipe() {
+  return <div>Todo: faire la recette</div>;
+}
+
+/**
+ * Export
+ */
+export default Recipe;
